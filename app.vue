03bade3f7f6ed1c010bf9ae243be15860f0a9410
SviAnL <402661663@qq.com>
2025-02-07 17:21:27 +0800

    init project

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome title="awdnka" />
+    <NuxtPage  />
+  </div>
+</template>
+<script setup>
+const appConfig = useAppConfig();
+console.log("appConfig111", appConfig);
+</script>
